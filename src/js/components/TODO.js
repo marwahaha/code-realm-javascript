fd6caf5c49dc190d0c893cc47dbf9f4ad95e75be
Aditya Prakash Sharma <adityaprakashsharma.84@gmail.com>
2019-07-06 04:04:46 -0500

    Updated TODO

--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,39 @@
-// Code for the TODO app goes here.
+// Code for the TODO app goes her
+
+var toDoDisplayList = (function(){
+    var toDoListToDisplay = toDOStore.getToDoList();
+
+    function getListToDisplay (list){
+         toDoListToDisplay = list;
+         if(toDoListToDisplay){
+           var htmlToAppend = ''
+           for(let i =0 ;i<toDoListToDisplay.length;i++){
+             if(toDoListToDisplay[i]){
+               htmlToAppend += '<li>'+toDoListToDisplay[i].description+'<input type="button" value="Delete Task" onclick = "deleteTask();"></li>';
+               
+            }
+          }
+            document.getElementById('taskList').innerHTML = htmlToAppend;
+         }
+    };
+
+    function addTask(){
+        var description = document.getElementById('todoTask').value;
+        toDoListToDisplay = toDOStore.addItemToDoList(toDoListToDisplay,description);
+        getListToDisplay(toDoListToDisplay);
+    }
+
+     function deleteTask(){
+        var description = document.getElementById('todoTask').value;
+        toDoListToDisplay = toDOStore.addItemToDoList(toDoListToDisplay,description);
+        getListToDisplay(toDoListToDisplay);
+    }
+
+    return {
+      getListToDisplay: getListToDisplay,
+      addTask : addTask,
+      deleteTask: deleteTask
+
+
+    }
+}) ();
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,80 @@
 // The core logic of the store will go here.
+var toDOStore = function (){
+  var toDoList = [] ;
+  var item = {
+      'id':'',
+      'description':'',
+      'state':''
+  };
+
+  function getToDoList () {
+   return toDoList;
+  };
+
+/*  function getToDoItem (item){
+    for(let x in toDoList){
+      if(toDoList[x].id === item.id){
+        return toDoList[x];
+      }
+
+    }
+  };
+
+  function setToDoItem (item){
+   for(let x in toDoList){
+      if(toDoList[x].id === item.id){
+        return toDoList.push(item);
+      }
+
+    }
+  };*/
+
+  function addItemToDoList (updatedList,item){
+    var itemId = 0;
+    if(updatedList){
+        itemId = updatedList.length+1;
+    }
+     return [].concat(updatedList, {'id':itemId,'description': item,'state':'isOpen'})
+    
+
+
+//      toDoList.push({'id':toDoList.length+1,'description': item,'state':'isOpen'});
+  };
+
+  function removeItemToDoList (updatedList,item){
+     return updatedList.slice(0,item.id).concat(updatedList.slice(item.id+1));
+  };
+
+  function setTaskToCompleted (item){
+    for(let x in toDoList){
+      if(toDoList[x].id === item.id){
+        toDoList[x].state = 'isCompleted';
+        return toDoList;
+      }
+
+    }
+  }
+
+    function setTaskToOpen (item){
+    for(let x in toDoList){
+      if(toDoList[x].id === item.id){
+        toDoList[x].state = 'isOpen';
+        return toDoList;
+      }
+
+    }
+  }
+
+  return {
+    getToDoList: getToDoList,
+
+    setTaskToOpen: setTaskToOpen,
+    setTaskToCompleted: setTaskToCompleted,
+    addItemToDoList: addItemToDoList,
+    removeItemToDoList: removeItemToDoList
+
+
+  }
+
+
+}();
