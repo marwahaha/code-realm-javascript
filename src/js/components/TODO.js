d946f97f6256bf25527131b3932ee290497828f4
= <=>
2019-07-06 14:51:10 +0530

    Todo Javascript Application

--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,46 @@
 // Code for the TODO app goes here.
+
+(function() {
+    var TodoStore = require('../store/store').TodoStore;
+        
+        var Todo = function(){
+            this.todos = [];
+            var todoId = document.getElementById('todos');
+            this.fetchTodos = ()=>{
+               this.todos = this.TodoStore.fetchTodos(); 
+               this.displayTodos();
+            }
+            this.addTodo = ()=>{
+                let todo = {
+                    "name":document.getElementById('todoName').value,
+                    "description":document.getElementById('todoDescription').value,
+                    "date":document.getElementById('todoDate').value,
+                    "status":"Open"
+                }
+                this.TodoStore.addTodo(todo);
+                this.fetchTodos();
+            }
+            this.deleteTodo = (id)=>{
+
+            }
+            this.markUnmarkTodo = (id,status)=>{
+
+            }
+
+            this.displayTodos = ()=>{
+                let html = "";
+                if(this.todos.length>0)
+                for(let todo of this.todos)
+                {
+                    html+=`<div>
+                        `+todo.name+`&nbsp;&nbsp;<p>`+todo.description+`</p>&nbsp;&nbsp;`+todo.date+`&nbsp;&nbsp;<button type="button" onclick="markUnmarkTodo('`+todo.id+`','`+todo.status+`')" title="Mark/Unmark Todo ">`+todo.status+`</button>
+                        &nbsp;&nbsp;<button type="button" onclick="deleteTodo('`+todo.id+`','`+todo.status+`')" title="Delete Todo ">Delete</button>
+                        </div>`
+                }
+                else
+                html="<p>Sorry No todos available</p>"   
+                this.todoId.innerHtml = html
+            }
+        }
+        Todo.fetchTodos();
+  }());
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,61 @@
 // The core logic of the store will go here.
+
+var TodoStore = function(){
+    var todos;
+    var initStore = function()
+    {
+       this.todos = [];
+    } 
+
+    var addTodo = function(todo)
+    {
+        
+        var date = new Date()
+        var todoData = 
+        {
+            "id":date.getTime(),
+            "name":todo.name,
+            "status":todo.status,
+            "date":todo.date,
+            "description":todo.description
+        }
+        this.todos.push(todoData)
+    }
+
+    var fetchTodo = function(id)
+    {
+        return this.todos.filter(function(todo,index,todos){
+                if(id==todos[index].id)
+                return todo;
+        })
+    }
+
+    var fetchTodos = function()
+    {
+        return this.todos;
+    }
+
+    var markUnmarkTodo = function(id,status)
+    {
+        var todoData = this.todos.filter(function(todo,index,todos){
+            if(id==todos[index].id)
+            return {index:index,todo:todo};
+        })
+        this.todos[todoData.index].status = status;
+        return this.todos[todoData.index];
+    }
+
+    var deletTodo = function(id)
+    {
+        var todoData = this.todos.filter(function(todo,index,todos){
+            if(id==todos[index].id)
+            return {index:index,todo:todo};
+        })
+        
+        return delete this.todos[todoData.index];
+    }
+
+   
+}    
+
+module.exports.TodoStore = TodoStore;
