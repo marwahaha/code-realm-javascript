06dd3ce29039d83864bd7d8f1ac64d11073eaeaa
sonampanjabi <sonampanjabi113@gmail.com>
2019-07-06 14:26:18 +0530

    Update store.js

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,19 @@
 // The core logic of the store will go here.
+// The core logic of the store will go here.
+var appState = {
+    todoTasks: [],
+    completedTaks: []
+  };
+  
+  function getState() {
+    var state = Object.assign({}, appState);
+    return state;
+  }
+  
+  function changeStateObj(key, value) {
+    
+    if(!appState.hasOwnProperty(key))
+      return;
+      
+    appState[key] = value;
+  }
