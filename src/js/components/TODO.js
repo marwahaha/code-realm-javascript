2c10f1dcc78c7aa524fffe00cef9d8cffd6d4ee3
Rahul mandloi <luharm03@gmail.com>
2019-07-06 14:28:21 +0530

    todo app

--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,47 @@
 // Code for the TODO app goes here.
+const addTask = () => {
+
+    let task = document.getElementById('newtask').value;
+    addtoStore(task);
+    document.getElementById('newtask').value = null;
+    console.log(store);
+    onload();
+};
+
+const moveTask = (id, status) => {
+    changeState(id, status);
+    onload();
+};
+
+const listOpenTask = () => {
+    let list = '';
+    Object.keys(store).map(key => {
+        if (store[key].status == "OPEN") {
+            list += `<div><span>${store[key].name}</span>  <span class="btn btn-outline-primary" onClick=moveTask('${store[key].id}','DONE')>Move to Done</span> </div>`;
+        }
+    });
+    if(list == '') {list = '<div>No Task available</div>';}
+    document.getElementById("opentask").innerHTML = list;
+}
+
+const listDoneTask = () => {
+    let list = '';
+    Object.keys(store).map(key => {
+        if (store[key].status == "DONE") {
+            list += `<div><span>${store[key].name}</span> <span class="btn btn-outline-primary" onClick=moveTask('${store[key].id}','OPEN')>Move to Open</span> </div>`;
+        }
+    });
+
+    if(list == '') {list = '<div>No Task available</div>';}
+    document.getElementById('donetask').innerHTML = list;
+}
+
+listOpenTask();
+listDoneTask();
+
+const onload = () => {
+    listOpenTask();
+    listDoneTask();
+}
+
+onload();
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,24 @@
-// The core logic of the store will go here.
+// The core logic of the store will go here.// The core logic of the store will go here.
+
+const store = {};
+const addtoStore = (task) => {
+    let state = {...store};
+    let date = new Date(); 
+    let timestamp = date.getTime();
+    let tempTask = {
+        id:timestamp,
+        name: task,
+        status: "OPEN",
+        user:"DEMO"
+    }
+    state =  Object.assign(state,{[timestamp]:tempTask});
+    store[timestamp] = tempTask;    
+};
+
+const changeState = (id, status) => {
+    //const state ={...store, store[id]:{...store[id],status:status}};
+  //  let state = {...store};
+   // store =  Object.assign({},...store,{...store[id],status:status});
+   // console.log(state);
+    store[id].status = status;   
+};
