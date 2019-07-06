a5ba31f9b74b998c73e55c26bb1094163d11a128
Pavan <pavan.shukla2008@gmail.com>
2019-07-06 13:30:45 +0530

    custom-store

--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,19 @@
 // Code for the TODO app goes here.
+import Store from 'store.js';
+
+let store = new Store();
+
+store.subscribe(function(state) {
+    console.log(state);
+});
+
+let action = {
+    type: 'ADD_TODO',
+    payload: {
+        todoTask: {
+            description: 'I need to create custom redux store',
+            completed: false
+        }
+    }
+}
+store.dispatch(action);
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,64 @@
 // The core logic of the store will go here.
+
+    let subscribers = [];
+    
+    function Store() {
+        this.state = {};
+
+        this.state = this.reduce(this.state, { todos: [] });
+    }
+
+    Store.prototype.getState = function () {
+        return this.state;
+    };
+
+    Store.prototype.dispatch = function (action) {
+        this.state = this.reduce(this.state, action);
+
+        this.notifySubscribers();
+
+        return action;
+    };
+
+    Store.prototype.reduce = function (state, action) {
+        return {
+            todos: toDoReducer(state.todos, action)
+        };
+    };
+
+    Store.prototype.subscribe = function (fn) {
+        subscribers.push(fn);
+    };
+
+    Store.prototype.notifySubscribers = function () {
+        subscribers.forEach(function (subscriber) {
+            subscriber(this.state);
+        }.bind(this));
+    };
+
+    function toDoReducer(todos, action) {
+        switch(action.type) {
+            case 'ADD_TODO':
+                if(Object.prototype.toString.call(todos) !== "[object Array]") {
+                    todos = [];
+                }
+                todos.push(action.payload.todoTask);
+                break;
+
+            case 'MARK_AS_COMPLETED':
+                todos[action.payload.todoIndex].completed = true;
+                break;
+
+            case 'MARK_AS_OPEN':
+                todos[action.payload.todoIndex].completed = false;
+                break;
+
+            case 'DELETE_TODO':
+                delete todos[action.payload.todoIndex];
+                break; 
+        };
+
+        return todos;
+    }
+
+    module.exports = Store;
