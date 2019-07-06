1fc11acd4165c325f4e6edc36adb071a1b851b20
Vijay Gaikwad <vijay.gaikwad@codeastu.com>
2019-07-06 14:35:15 +0530

    Incomplete todo list app.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TODO from "./js/components/TODO";
+function App() {
+  return (
+    <div className="App">
+      <TODO />
+    </div>
+  );
+}
+
+export default App;
--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,35 @@
 // Code for the TODO app goes here.
+import React, { Component } from "react";
+class TODO extends Component {
+  state = {
+    newTask: "",
+    task:{}
+  };
+
+  onInputChange(event = { target: { name: "", value: "" } }) {
+    const { name, value } = event.target;
+    let newState = {};
+    newState[name] = value;
+    this.setState(Object.assign({}, this.state, newState));
+  }
+  handleAddItemClick = () => {
+    this.setState(Object.assign({}, this.state, this.state.newTask));
+  };
+  render() {
+    return (
+      <div>
+        <h1>My Todo-list</h1>
+        <input
+          id="newTask"
+          name="newTask"
+          placeholder="Add your taks..."
+          value={this.state.newTask}
+          onChange={this.onInputChange.bind(this)}
+        />
+        <button onClick={this.handleAddItemClick.bind(this)}>Add</button>
+        <ul id="taskList">{this.state.task}</ul>
+      </div>
+    );
+  }
+}
+export default TODO;
