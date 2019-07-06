8ded0839fdb3cf157413af18d0198ee4d5fe9443
Bhushan Asolkar <bhushan.asolkar@yahoo.com>
2019-07-06 13:34:52 +0530

    JS assignment - bhushan-asolkar

--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,44 @@
 // Code for the TODO app goes here.
+let todosList;
+function populateTodos() {
+    // let todosList = [{ name: 'TODO: 1', isOpen: true }];
+    todosList = getState({ action: 'DISPLAY_TODOS_ONLOAD' });
+    updateUI();
+}
+
+function setOpenOrClosed(id) {
+    const todoItem = todosList.find(todo => todo.id === id);
+    todoItem.isOpen = !todoItem.isOpen;
+    todoList = getState({ action: 'TODO_UPDATED', data: todoItem });
+    updateUI();
+}
+
+
+function deleteTodo(id) {
+    todosList = getState({ action: 'TODO_DELETED', data: id });
+    updateUI();
+}
+
+function addTodo() {
+    let todoNameElem = document.getElementById('todoName');
+    todosList = getState({ action: 'TODO_ADDED', data: todoNameElem.value });
+    todoNameElem.value = '';
+    updateUI();
+}
+
+function updateUI() {
+    let listElem = document.getElementById('list');
+    listElem.innerHTML = '';
+    for (let todo of todosList) {
+        listElem.innerHTML += (todo.isOpen) ? `
+        <span>${todo.name}</span>
+        <span><input type="checkbox" checked onclick="setOpenOrClosed(${todo.id})"></span>
+        <button type="button" onclick="deleteTodo(${todo.id})">Delete</button><br>
+        ` :
+        `
+        <span>${todo.name}</span>
+        <span><input type="checkbox" onclick="setOpenOrClosed(${todo.id})"></span>
+        <button type="button" onclick="deleteTodo(${todo.id})">Delete</button><br>
+        `;
+    }
+}
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,40 @@
 // The core logic of the store will go here.
+function filterOpenTODOS(arr) {
+    return arr.filter(obj => obj.isOpen);
+}
+const initialState = [{
+    id: 1,
+    name: 'Todo 1',
+    isOpen: true
+}];
+let currentState;
+let idx;
+function getState(payload) {
+    switch(payload.action) {
+        case 'DISPLAY_TODOS_ONLOAD':
+            currentState = initialState;
+            console.log('ACTION:', payload.action);
+            console.log('state', currentState);
+            break;
+        case 'TODO_UPDATED':
+            idx = currentState.findIndex(c => c.id === payload.data.id);
+            currentState.splice(idx, 1, payload.data);
+            console.log('ACTION:', payload.action);
+            console.log('state', currentState);
+            break;
+        case 'TODO_DELETED':
+            idx = currentState.findIndex(c => c.id === payload.data);
+            currentState.splice(idx, 1);
+            console.log('ACTION:', payload.action);
+            console.log('state', currentState);
+            break;
+        case 'TODO_ADDED':
+            const newTODO = { id: currentState[currentState.length - 1].id + 1, name: payload.data, isOpen: true };
+            currentState.push(newTODO);
+            console.log('ACTION:', payload.action);
+            console.log('state', currentState);
+            break;
+    }
+    const newState = filterOpenTODOS(currentState);
+    return JSON.parse(JSON.stringify(newState));
+}
