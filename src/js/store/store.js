52e245500686048f65e188843b3411be792e98fc
Ruchita Mehrotra <ruchita.mehrotra456@gmail.com>
2019-07-06 14:30:53 +0530

    code for ruchita-mehrotra-javascript-test

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,44 @@
 // The core logic of the store will go here.
+
+
+const todoList =($ctrl) =>{
+$ctrl.todoList={};
+if($ctrl.state.status.responseCode==='200'){
+if($ctrl.state.data.todoList){
+$ctrl.todoList=$ctrl.state.data.todoList;
+return $ctrl.todoList;
+};
+};
+};
+
+
+const addTodoTask=($ctrl, item) =>{
+if(item){
+for(let prop in $ctrl.todoList{
+ 
+   if($ctrl.todoList.hasOwnProperty(prop)){
+ 
+       $ctrl.todoList[prop]=item[prop];
+	$ctrl.todoList['Completed']=true;
+};
+};
+return $ctrl.todoList;
+};
+
+
+
+const deleteTodoTask=($ctrl,index)=>{
+if(index){
+var todoList=$ctrl.todoList;
+delete todoList[index];
+$ctrl.todoList=todoList;
+return $ctrl.todoList;
+};
+};
+
+
+
+
+
+
+
