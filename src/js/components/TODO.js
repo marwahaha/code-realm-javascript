de872f8984029efa8019bd070826653e5c5f62da
manshis <ece.manshi@gmail.com>
2019-07-06 14:29:17 +0530

    intal commit

--- src/js/components/TODO.js
+++ src/js/components/TODO.js
@@ -1,1 +1,51 @@
-// Code for the TODO app goes here.
+getList();
+function addTask() {
+  const taskName = document.getElementById("task-name").value;
+  addInStore(taskName);
+  getList();
+  document.getElementById("task-name").value = "";
+}
+
+function getList() {
+  const toDoList = listStore();
+  const toDoListDiv = document.getElementById("to-do-list");
+  toDoList.forEach(todoItem => {
+    if (!todoItem.added) {
+      console.log(todoItem);
+      const toDoItemDiv = document.createElement("div");
+      const toDoItemButton = document.createElement("button");
+      const toDoItemDelButton = document.createElement("button");
+
+      toDoItemDiv.textContent = todoItem.name;
+      toDoItemDiv.setAttribute("id", todoItem.name);
+
+      toDoItemButton.textContent = "Mark as completed";
+      toDoItemButton.onclick = function() {
+        markAsCompleted(todoItem.name);
+      };
+      toDoItemDelButton.textContent = "Delete";
+      toDoItemDelButton.onclick = function() {
+        deleteTask(todoItem.name);
+      };
+      toDoItemDiv.appendChild(toDoItemButton);
+      toDoItemDiv.appendChild(toDoItemDelButton);
+
+      toDoListDiv.appendChild(toDoItemDiv);
+      todoItem.added = true;
+    } else {
+      const toDoItemtDiv = document.getElementById(todoItem.name);
+      if (todoItem.status == "open") {
+        toDoItemtDiv.childNodes[1].textContent = "Mark as completed";
+      } else {
+        toDoItemtDiv.childNodes[1].textContent = "Open";
+      }
+    }
+  });
+}
+
+function markAsCompleted(name) {
+  changeStatus(name);
+  getList();
+}
+
+function deleteTask(name) {}
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,1 +1,30 @@
 // The core logic of the store will go here.
+
+const store = [
+  {
+    name: "Test",
+    status: "open",
+    added: false
+  }
+];
+
+function listStore() {
+  return store;
+}
+
+function addInStore(task) {
+  store.push({
+    name: task,
+    status: "open",
+    added: false
+  });
+}
+
+function changeStatus(taskName) {
+  const storeItem = store.find(item => item.name == taskName);
+  if (storeItem.status == "open") {
+    storeItem.status = "completed";
+  } else {
+    storeItem.status = "open";
+  }
+}
